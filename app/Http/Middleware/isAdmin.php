70a40be77acfc111d088dda1578b875f2e10a2bd
= <=>
2022-08-06 18:09:20 +0800

    create a migrate is_admin is not create new table but this modify table structure

---
+++ app/Http/Middleware/isAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class isAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // Jika belum login / bukan admin
+        if (!auth()->check() || auth()->user()->username != "fauzi-co") {
+            // Pesan Forbidden di Browser
+            abort(403);
+        }
+
+        return $next($request);
+    }
+}

a19e3f3c65c7fd7d918d2eac7c5bedcf7afdd2e5
= <=>
2022-08-06 14:42:50 +0800

    edit file /dashboard/categories/index

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -2,41 +2,39 @@
 
 @section('container')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-  <h1 class="h2">Welcome Post</h1>
+  <h1 class="h2">Post Categories</h1>
 </div>
 
 @if (session()->has('success'))
-<div class="alert alert-info" role="alert">
+<div class="alert alert-info col-lg-6" role="alert">
   {{ session('success') }}
 </div>  
 @endif
 
-<a href="/dashboard/posts/create" class="btn btn-primary btn-sm mb-3">Create New Post</a>
+<a href="/dashboard/categories/create" class="btn btn-primary btn-sm mb-3">Create New Category</a>
 
-<div class="table-responsive col-lg-10">
+<div class="table-responsive col-lg-6">
   <table class="table table-striped table-sm">
     <thead>
       <tr>
         <th scope="col">#</th>
-        <th scope="col">Title</th>
-        <th scope="col">Category</th>
+        <th scope="col">Category Name</th>
         <th scope="col">Action</th>
       </tr>
     </thead>
     <tbody>
-      @foreach ($posts as $post)
+      @foreach ($categories as $category)
       <tr>
         <td>{{ $loop->iteration }}</td>
-        <td>{{ $post->title }}</td>
-        <td>{{ $post->category->name }}</td>
+        <td>{{ $category->name }}</td>
         <td>
-          <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info">
+          <a href="/dashboard/categories/{{ $category->slug }}" class="badge bg-info">
             <span data-feather="eye"></span>
           </a> 
-          <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning">
+          <a href="/dashboard/categories/{{ $category->slug }}/edit" class="badge bg-warning">
             <span data-feather="edit"></span>
           </a> 
-          <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline" onsubmit="return confirm('Are you sure?')">
+          <form action="/dashboard/categories/{{ $category->slug }}" method="POST" class="d-inline" onsubmit="return confirm('Are you sure?')">
             @method('delete')
             @csrf
             <button type="submit" class="border-0 badge bg-danger">
